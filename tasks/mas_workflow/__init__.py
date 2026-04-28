c34c223299a17cf4639a859a8abc21381f069ba0
f-rower <fran_gt6@hotmail.com>
2026-04-28 12:00:17 +0100

    added autogen pddl script that can be run through mas_type in bash script. Still to be modified with custom MAS setup

--- tasks/mas_workflow/__init__.py
+++ tasks/mas_workflow/__init__.py
@@ -2,11 +2,13 @@ from mas.mas import MetaMAS
 from .autogen import AutoGen
 from .macnet import MacNet
 from .dylan import DyLAN
+from .autogen.autogen_pddl import AutoGen as AutoGenPDDL
 
 MAS = {
     'autogen': AutoGen,
     'macnet': MacNet,
-    'dylan': DyLAN
+    'dylan': DyLAN,
+    'autogen_pddl': AutoGenPDDL
 }
 
 def get_mas(mas_type: str) -> MetaMAS:
--- tasks/mas_workflow/autogen/__init__.py
+++ tasks/mas_workflow/autogen/__init__.py
@@ -1,5 +1,7 @@
 from .autogen import AutoGen
+from .autogen_pddl import AutoGen as AutoGenPDDL
 
 __all__ = [
     "AutoGen",
+    "AutoGenPDDL"
 ]
